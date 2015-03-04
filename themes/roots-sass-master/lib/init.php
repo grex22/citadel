92fad5cbcdb2b171c97350ceb250047890a0fa45
Greg Burkett <greg@ripfish.com>
2015-03-04 17:38:21 -0500

    content styles, code tweaks

--- themes/roots-sass-master/lib/init.php
+++ themes/roots-sass-master/lib/init.php
@@ -41,8 +41,8 @@ function roots_widgets_init() {
     'id'            => 'sidebar-primary',
     'before_widget' => '<section class="widget %1$s %2$s">',
     'after_widget'  => '</section>',
-    'before_title'  => '<h3>',
-    'after_title'   => '</h3>',
+    'before_title'  => '<h4>',
+    'after_title'   => '</h4>',
   ));
 
   register_sidebar(array(
